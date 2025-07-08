5c2e7bae9ebb2ffe6d3d2fe282d2f4fb1ab5037c
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-08 11:18:03 +0500

    initial commit

--- config/settings.py
+++ config/settings.py
@@ -51,14 +51,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
