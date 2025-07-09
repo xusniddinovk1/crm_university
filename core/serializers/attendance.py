fda4e3598ba156b9aaafbe9bde0e4716a7e99ac1
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-09 17:31:32 +0500

    create serializer

--- core/serializers/attendance.py
+++ core/serializers/attendance.py
@@ -1,1 +1,15 @@
 from rest_framework import serializers
+
+
+class AttendanceSerializer(serializers.ModelSerializer):
+    STATUS = (
+        ('present', 'Keldi'),
+        ('late', 'Kechikdi'),
+        ('absent', 'Kelmadi')
+    )
+    student_id = serializers.IntegerField()
+    status = serializers.ChoiceField(choices=STATUS)
+
+
+class GroupAttendanceGETResponseSerializer(serializers.Serializer):
+    attendance_data = AttendanceSerializer(many=True)
