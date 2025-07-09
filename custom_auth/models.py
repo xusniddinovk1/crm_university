e1af3311872e02242f1018dbe269ff2e2139fefa
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-09 08:38:16 +0500

    update custom user

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -13,19 +13,25 @@ class CustomUserManager(BaseUserManager):
 
     def creates_superuser(self, phone_number, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_admin', True)
         if extra_fields.get('is_staff') is not True:
             raise ValueError('The user must be staff')
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('The user must be staff')
+        if extra_fields.get('is_admin') is not True:
+            raise ValueError('The user must be superuser')
         return self.create_user(phone_number, password, **extra_fields)
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=13, unique=True)
     data_joined = models.DateTimeField(auto_now_add=True)
+    group = models.ForeignKey('GroupStudent', on_delete=models.SET_NULL, null=True, blank=True,
+                              related_name='students_in_group')
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
 
     objects = CustomUserManager()
 
@@ -34,3 +40,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.phone_number
+
+    @property
+    def is_superuser(self):
+        return self.is_admin
